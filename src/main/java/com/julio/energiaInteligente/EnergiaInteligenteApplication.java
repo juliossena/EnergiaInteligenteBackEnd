2eb3eccaf2a78390644feb36fd4779a4ef9ecbf7
juliossena <juliosouzasena@gmail.com>
2018-08-19 15:03:05 -0300

    inicio projeto

---
+++ src/main/java/com/julio/energiaInteligente/EnergiaInteligenteApplication.java
@@ -0,0 +1,21 @@
+package com.julio.energiaInteligente;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class EnergiaInteligenteApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergiaInteligenteApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		
+
+	}
+}
