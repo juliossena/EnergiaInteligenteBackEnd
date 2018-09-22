895efc8a92f4371355bb5c783e2164683cd411b5
juliossena <juliosouzasena@gmail.com>
2018-09-22 16:04:32 -0300

    Melhorando velocidade lista medicao

--- src/main/java/com/julio/energiaInteligente/domain/Circuito.java
+++ src/main/java/com/julio/energiaInteligente/domain/Circuito.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -36,7 +37,7 @@ public class Circuito implements Serializable {
 	private ConfiguracaoCircuito configuracaoCircuito;
 
 	@JsonIgnore
-	@OneToMany(mappedBy = "circuito")
+	@OneToMany(mappedBy = "circuito", fetch = FetchType.LAZY)
 	private List<Medicao> medicoes = new ArrayList<>();
 
 	@OneToMany(mappedBy = "circuito")
